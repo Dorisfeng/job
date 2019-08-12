0f23576aa331b9c67a2b7ce5748663336235cb1c
封多多 <2276172014@qq.com>
2019-08-12 19:39:32 +0800

    change

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(function(){
-	$(".nav ul li").click(function(e){	
+    $(".nav ul li").click(function (e) {	
+        
         $('.nav ul li a').removeClass("active");	
         $(e.target).addClass('active');
         let index = $('.nav ul li').index($(e.target).parent());
