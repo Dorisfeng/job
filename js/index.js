676cf1b8eea00bf9db95070dd6f295c2642032a3
封多多 <2276172014@qq.com>
2019-08-24 15:50:57 +0800

    navbar响应式布局

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 $(function(){
-    $(".header ul li").click(function (e) {	
+    // $(".header ul li").click(function (e) {	
+    //     console.log(e.target)
+    //     $('.header ul li ').removeClass("active");	
+    //     $(e.target).addClass('active');
+    //     let index = $('.header ul li').index(e.target);
+    //     $('.content>div').eq(index).show().siblings().hide()
+
+	// })
+ $(".page-header ul li").click(function (e) {	
         console.log(e.target)
-        $('.header ul li ').removeClass("active");	
+        $('.page-header ul li a').removeClass("active");	
         $(e.target).addClass('active');
-        let index = $('.header ul li').index(e.target);
+        let index = $('.page-header ul li').index($(e.target).parent());
         $('.content>div').eq(index).show().siblings().hide()
 
 	})
-	
 
 	
 })
